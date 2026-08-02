083550e484a3db7385c969ee7f920e14bcb69874
foosy <foosy>
2026-08-03 02:22:45 +0800

    Fix js_runtimes python format

--- video_fetcher.py
+++ video_fetcher.py
@@ -26,7 +26,7 @@ def get_latest_video(search_query, max_results=10, download_dir="downloads"):
             }
         },
         'source_address': '0.0.0.0', # Force IPv4
-        'js_runtimes': ['node'], # Enable PO Token generation
+        'js_runtimes': {'node': {}}, # Enable PO Token generation
         'remote_components': ['ejs:github'],
     }
     
