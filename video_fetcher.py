135f44812d4b9319805d330d53a2cb6a7abd1df7
foosy <foosy>
2026-08-03 02:39:18 +0800

    Force yt-dlp to use cookies.txt

--- video_fetcher.py
+++ video_fetcher.py
@@ -21,7 +21,7 @@ def get_latest_video(search_query, max_results=10, download_dir="downloads"):
         'skip_download': False,
         'noplaylist': True,
         'quiet': False,
-        'cookiefile': 'cookies.txt' if os.path.exists('cookies.txt') else None,
+        'cookiefile': 'cookies.txt',
         'ffmpeg_location': imageio_ffmpeg.get_ffmpeg_exe(),
         'extractor_args': {
             'youtube': {
